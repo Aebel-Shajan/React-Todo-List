4e0980c371157ced5f8ecfe9a58bb29137ba6b2b
Aebel Shajan <aebelshajan@gmail.com>
2023-12-01 20:51:14 +0000

    Finished MDN Tutorial React 5 - React interactivity: Editing, filtering, conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,10 +5,27 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+	All: () => true,
+	Active: (task) => !task.completed,
+	Completed: (task) => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 
 
 function App(props) {
 	const [tasks, setTasks] = useState(props.tasks);
+	const [filter, setFilter] = useState("All");
+
+	const filterList = FILTER_NAMES.map((name) => (
+		<FilterButton 
+			key={name} 
+			name={name} 
+			isPressed={name===filter}
+			setFilter={setFilter}
+			/>
+	))
 
 	function addTask(name) {
 		const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -30,8 +47,20 @@ function App(props) {
 		setTasks(remainingTasks);
 	}
 
+	function editTask(id, newName) {
+		const editedTaskList = tasks.map((task) => {
+			if (id===task.id) {
+				return {...task, name: newName};
+			}
+			return task;
+		});
+		setTasks(editedTaskList);
+	}
+
 	// The ?. operator lets us perform optional chaining to check if props.tasks is undefined or null before attempting to create a new array of task names
-	const taskList = tasks.map((task) => (
+	const taskList = tasks
+		.filter(FILTER_MAP[filter])
+		.map((task) => (
 		<Todo
 			id={task.id}
 			name={task.name}
@@ -39,6 +68,7 @@ function App(props) {
 			key={task.id}
 			toggleTaskCompleted={toggleTaskCompleted}
 			deleteTask={deleteTask}
+			editTask={editTask}
 		/>
 	));
 	const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
@@ -48,10 +78,7 @@ function App(props) {
 			<h1>TodoMatic</h1>
 			<Form addTask={addTask} />
 			<div className="filters btn-group stack-exception">
-				<FilterButton />
-				<FilterButton />
-				<FilterButton />
-
+				{filterList}
 			</div>
 			<h2 id="list-heading">{headingText}</h2>
 
