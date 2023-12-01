a8e94193c78e84421858bd95cc5ae71c5bf40251
Aebel Shajan <aebelshajan@gmail.com>
2023-12-01 20:07:07 +0000

    Finished MDN Tutorial React 4 - React interactivity: Events and state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
-import React from "react"
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import React, { useState } from "react"
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid";
+
+
 
 function App(props) {
+	const [tasks, setTasks] = useState(props.tasks);
+
+	function addTask(name) {
+		const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+		setTasks([...tasks, newTask]);
+	}
+
+	function toggleTaskCompleted(id) {
+		const updatedTasks = tasks.map((task) => {
+			if (id===task.id) {
+				return { ...task, completed: !task.completed}
+			}
+			return task;
+		});
+		setTasks(updatedTasks);
+	}
+
+	function deleteTask(id) {
+		const remainingTasks = tasks.filter((task)=> id !==task.id);
+		setTasks(remainingTasks);
+	}
+
 	// The ?. operator lets us perform optional chaining to check if props.tasks is undefined or null before attempting to create a new array of task names
-	const taskList = props.tasks?.map((task) => (
+	const taskList = tasks.map((task) => (
 		<Todo
 			id={task.id}
 			name={task.name}
 			completed={task.completed}
 			key={task.id}
+			toggleTaskCompleted={toggleTaskCompleted}
+			deleteTask={deleteTask}
 		/>
 	));
-	console.log(props);
+	const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+	const headingText = `${taskList.length} ${tasksNoun} remaining`;
 	return (
 		<div className="todoapp stack-large">
 			<h1>TodoMatic</h1>
-			<Form />
+			<Form addTask={addTask} />
 			<div className="filters btn-group stack-exception">
 				<FilterButton />
 				<FilterButton />
 				<FilterButton />
 
 			</div>
-			<h2 id="list-heading">3 tasks remaining</h2>
+			<h2 id="list-heading">{headingText}</h2>
 
 			<ul
 				role="list"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Form(props) {
+	/* 
+	1. We are setting the initial name value as "Use hooks!".
+	2. We are defining a function whose job is to modify name, called setName().
+	3. useState() returns these two things, so we are using array destructuring to capture them both in separate variables.
+	*/
+	const [name, setName] = useState("");
+	
+	function handleChange(e) {
+		setName(e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (name.length > 0) {
+			props.addTask(name);
+		}
+		setName("");
+	}
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<h2 className="label-wrapper">
 				<label htmlFor="new-todo-input" className="label__lg">
 					What needs to be done?
@@ -14,6 +34,8 @@ function Form(props) {
 				className="input input__lg"
 				name="text"
 				autoComplete="off"
+				value={name}
+				onChange={handleChange}
 			/>
 			<button type="submit" className="btn btn__primarybtn__lg">
 				Add
